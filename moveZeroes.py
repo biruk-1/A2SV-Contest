6123b2f64f44a6873c3450e279d233f5533bbf93
biruk-1 <99360145+biruk-1@users.noreply.github.com>
2024-04-09 02:28:20 -0500

    Update moveZeroes.py

--- moveZeroes.py
+++ moveZeroes.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        holder = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[holder],nums[i] = nums[i],nums[holder]
+                holder+=1
